5b1e6f4f595f2da7511cff2005f6b2827bc9b252
Murage-Ivy <ivymurage2000@gmail.com>
2023-06-24 17:02:44 +0300

    FInd first duplicate

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,16 +1,31 @@
 function findFirstDuplicate(arr) {
+  let result = -1;
+  if (arr.length === 0) {
+    result;
+  }
   // type your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== arr.indexOf(arr[i])) {
+      return arr[i];
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log("=>", findFirstDuplicate([2, 1, 4, 3, 2, 3, 4, 5]));
 
   console.log("");
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("");
+  console.log("=>", findFirstDuplicate([1, 2, 3, 4, 4]));
+
+  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2, 4]));
 }
 
 module.exports = findFirstDuplicate;

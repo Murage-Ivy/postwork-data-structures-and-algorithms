bca9510aa804362860df043318584ac20cfe7621
Murage-Ivy <ivymurage2000@gmail.com>
2023-06-24 17:22:10 +0300

    FInd first duplicate

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -15,17 +15,27 @@ function findFirstDuplicate(arr) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 4, 3, 2, 3, 4, 5]));
+  console.log(findFirstDuplicate([2, 1, 3, 3, 2]));
 
   console.log("");
 
   console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+  console.log(findFirstDuplicate([1, 2, 3, 4]));
 
   console.log("");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4, 4]));
 
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2, 4]));
+  console.log("Expecting: -1");
+  console.log(findFirstDuplicate([]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log(findFirstDuplicate([5]));
+
+  console.log("");
+
+  console.log("Expecting: 7");
+  console.log(findFirstDuplicate([7, 1, 2, 3, 7]));
 }
 
 module.exports = findFirstDuplicate;
